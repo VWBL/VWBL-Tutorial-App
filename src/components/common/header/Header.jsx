79ffd6d00ae6275fd885ab4fdc577c331677ec36
bibitto <bibitto.crypto@gmail.com>
2023-01-13 23:40:26 +0900

    feat: add Header

---
+++ src/components/common/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+export const Header = () => {
+  return (
+    <header className={'Header'}>
+      <img src="VWBL_EDUCATION_LOGO.svg" alt="Header_Logo" />
+    </header>
+  );
+};
