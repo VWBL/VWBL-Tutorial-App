312197f70520f2e093c4caf99e04fcdd51f575d0
bibitto <bibitto.crypto@gmail.com>
2023-01-13 23:52:56 +0900

    chore: add export pages

---
+++ src/components/pages/detail/Detail.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Detail.css';
+
+export const Detail = () => {
+  return (
+    <div className="Detail-Container">
+      <text>Detail</text>
+    </div>
+  );
+};
