2f1bccf9015f536a2e5bbb8c9dfe9dec8b1be582
bibitto <bibitto.crypto@gmail.com>
2023-01-13 23:40:09 +0900

    feat: add Footer

---
+++ src/components/common/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css';
+
+export const Footer = () => {
+  return (
+    <footer className={'Footer'}>
+      <text className={'Footer-text'}>©︎ 2022 Ango-ya, LLC. All rights reserved.</text>
+    </footer>
+  );
+};
