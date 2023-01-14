43bbdce38a6e67a531c95d322740eb61be7f520d
bibitto <bibitto.crypto@gmail.com>
2023-01-14 17:39:49 +0900

    update: root page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home, Create, Detail } from './components/pages';
+import { Footer, Header } from './components/common';
+import { VwblContainer } from './container';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <VwblContainer.Provider>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route path={'/'} element={<Home />} />
+            <Route path={'/new/'} element={<Create />} />
+            <Route path={'/assets/:id'} element={<Detail />} />
+          </Routes>
+          <Footer />
+        </div>
+      </VwblContainer.Provider>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/pages/index.js
@@ -0,0 +1,3 @@
+export * from './home/Home';
+export * from './create/Create';
+export * from './detail/Detail';
