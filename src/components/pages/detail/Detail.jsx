27b2b6f28f8a586c6ff3aab684b3da19c519016d
bibitto <bibitto.crypto@gmail.com>
2023-01-17 11:25:17 +0900

    update: Detail page

--- src/components/pages/detail/Detail.jsx
+++ src/components/pages/detail/Detail.jsx
@@ -1,10 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { VwblContainer } from '../../../container';
+import { useDisclosure } from '../../../hook';
+import { decryptedImageData } from '../../../utils';
+import { BackButton, CustomLoading, FileViewer, Section, TransferModal } from '../../common';
 import './Detail.css';
 
 export const Detail = () => {
+  const [loadedNft, setLoadedNft] = useState();
+  const [isViewingThumbnail, setViewingDataType] = useState(true);
+  const { userAddress, mintedNfts, ownedNfts } = VwblContainer.useContainer();
+  const { isOpen, handleOpen } = useDisclosure();
+  const tokenId = Number(useParams().id);
+
+  const handleViewData = () => {
+    setViewingDataType((prev) => !prev);
+  };
+
+  const specifyNftData = (id) => {
+    return mintedNfts.find((nft) => nft.id === id) || ownedNfts.find((nft) => nft.id === id);
+  };
+
+  const loadNFTByTokenId = (id) => {
+    setTimeout(() => {
+      const targetNft = specifyNftData(id);
+      targetNft.decrypted_image = decryptedImageData;
+      targetNft.owner = userAddress;
+      setLoadedNft(targetNft);
+    }, 3000);
+  };
+
+  useEffect(() => {
+    loadNFTByTokenId(tokenId);
+  }, []);
+
+  if (!loadedNft) {
+    return (
+      <div style={{ height: 'calc(100vh - 180px)' }}>
+        <CustomLoading />
+      </div>
+    );
+  }
+
   return (
-    <div className="Detail-Container">
-      <span>Detail</span>
+    <div style={{ height: 'calc(100vh - 180px)', display: 'flex' }}>
+      <TransferModal open={isOpen} onClose={handleOpen} nft={loadedNft} />
+      {isViewingThumbnail ? (
+        <FileViewer url={loadedNft.image} alt="NFT" height={'100%'} width={'100%'} />
+      ) : (
+        <FileViewer url={loadedNft.decrypted_image} alt="NFT" height={'100%'} width={'100%'} />
+      )}
+      <div className="Detail-Container">
+        <div className="Data-Wrapper">
+          <Section title={'Title'} data={loadedNft.name} />
+          <Section title={'Description'} data={loadedNft.description} />
+          <Section title={'Owner'} data={loadedNft.owner} />
+        </div>
+        <div className="Button-Wrapper">
+          {isViewingThumbnail ? (
+            <button className="View-Button" onClick={handleViewData}>
+              View Data
+            </button>
+          ) : (
+            <button className="View-Button" onClick={handleViewData}>
+              Return to Thumbnail
+            </button>
+          )}
+          {userAddress === loadedNft.owner && (
+            <button className="Transfer-Button" onClick={handleOpen}>
+              TRANSFER
+            </button>
+          )}
+        </div>
+        <BackButton to={'/'} />
+      </div>
     </div>
   );
 };
