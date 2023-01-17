e18f9fd83c1afa0178ff50a6a3f22b9f9079eaea
bibitto <bibitto.crypto@gmail.com>
2023-01-17 11:22:14 +0900

    update: add fetch-nfts & nft state

--- src/container/vwbl-container.js
+++ src/container/vwbl-container.js
@@ -1,20 +1,35 @@
 import { createContainer } from 'unstated-next';
 import { useState } from 'react';
+import { testNfts } from '../utils';
 
 const useVWBL = () => {
   const [userAddress, setUserAddress] = useState('');
+  const [mintedNfts, setMintedNfts] = useState();
+  const [ownedNfts, setOwnedNfts] = useState();
   const connectWallet = () => {
-    setUserAddress('0x00000000000000000000000000000000');
+    setUserAddress('0x0000000000000000000000000000000000000000');
   };
 
   const disconnectWallet = () => {
     setUserAddress('');
+    setMintedNfts();
+    setOwnedNfts();
+  };
+
+  const fetchNFTs = () => {
+    setTimeout(() => {
+      setMintedNfts(testNfts.slice(0, 2));
+      setOwnedNfts(testNfts);
+    }, 2000);
   };
 
   return {
     userAddress,
+    mintedNfts,
+    ownedNfts,
     connectWallet,
     disconnectWallet,
+    fetchNFTs,
   };
 };
 
