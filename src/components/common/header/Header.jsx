75e90c88ba294a4ed30219ec59d2730935fd48fc
bibitto <bibitto.crypto@gmail.com>
2023-01-19 11:54:26 +0900

    update: add home link to header-logo

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 export const Header = () => {
   return (
-    <header className={'Header'}>
-      <img src="VWBL_EDUCATION_LOGO.svg" alt="Header_Logo" />
-    </header>
+    <Link to={'/'}>
+      <header className={'Header'}>
+        <img src="VWBL_EDUCATION_LOGO.svg" alt="Header_Logo" />
+      </header>
+    </Link>
   );
 };
