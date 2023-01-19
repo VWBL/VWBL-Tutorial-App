e9b0ac8ea4d66f7393624203fec60f5d019d772b
bibitto <bibitto.crypto@gmail.com>
2023-01-19 12:10:11 +0900

    refactor: arrange code for lesson

--- src/components/pages/detail/Detail.jsx
+++ src/components/pages/detail/Detail.jsx
@@ -1,46 +1,38 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { VwblContainer } from '../../../container';
 import { useDisclosure } from '../../../hooks';
 import { decryptedImageData } from '../../../utils';
 import { BackButton, CustomLoading, FileViewer, Section, TransferModal } from '../../common';
+import { testNfts } from '../../../utils';
 import './Detail.css';
 
 export const Detail = () => {
-  const [loadedNft, setLoadedNft] = useState();
+  const [decryptedNft, setDecryptedNft] = useState();
   const [isViewingThumbnail, setViewingDataType] = useState(true);
-  const { userAddress, mintedNfts, ownedNfts } = VwblContainer.useContainer();
+  const { userAddress } = VwblContainer.useContainer();
   const { isOpen, handleOpen } = useDisclosure();
   const tokenId = Number(useParams().id);
+
+  // Lesson-6
+  const fechDecryptedNftByTokenId = (id) => {
+    setTimeout(() => {
+      const targetNft = testNfts.find((nft) => nft.id === id);
+      targetNft.decrypted_image = decryptedImageData;
+      targetNft.owner = userAddress;
+      setDecryptedNft(targetNft);
+    }, 3000);
+  };
 
   const handleViewData = () => {
     setViewingDataType((prev) => !prev);
   };
 
-  const specifyNftData = useCallback(
-    (id) => {
-      return mintedNfts.find((nft) => nft.id === id) || ownedNfts.find((nft) => nft.id === id);
-    },
-    [mintedNfts, ownedNfts]
-  );
-
-  const loadNFTByTokenId = useCallback(
-    (id) => {
-      setTimeout(() => {
-        const targetNft = specifyNftData(id);
-        targetNft.decrypted_image = decryptedImageData;
-        targetNft.owner = userAddress;
-        setLoadedNft(targetNft);
-      }, 3000);
-    },
-    [specifyNftData, userAddress]
-  );
-
   useEffect(() => {
-    loadNFTByTokenId(tokenId);
-  }, [loadNFTByTokenId, tokenId]);
+    fechDecryptedNftByTokenId(tokenId);
+  }, [fechDecryptedNftByTokenId, tokenId]);
 
-  if (!loadedNft) {
+  if (!decryptedNft) {
     return (
       <div style={{ height: 'calc(100vh - 160px)' }}>
         <CustomLoading />
@@ -50,17 +42,17 @@ export const Detail = () => {
 
   return (
     <div style={{ height: 'calc(100vh - 160px)', display: 'flex' }}>
-      <TransferModal open={isOpen} onClose={handleOpen} nft={loadedNft} />
+      <TransferModal open={isOpen} onClose={handleOpen} nft={decryptedNft} />
       {isViewingThumbnail ? (
-        <FileViewer url={loadedNft.image} alt="NFT" height={'100%'} width={'100%'} />
+        <FileViewer url={decryptedNft.image} alt="NFT" height={'100%'} width={'100%'} />
       ) : (
-        <FileViewer url={loadedNft.decrypted_image} alt="NFT" height={'100%'} width={'100%'} />
+        <FileViewer url={decryptedNft.decrypted_image} alt="NFT" height={'100%'} width={'100%'} />
       )}
       <div className="Detail-Container">
         <div className="Data-Wrapper">
-          <Section title={'Title'} data={loadedNft.name} />
-          <Section title={'Description'} data={loadedNft.description} />
-          <Section title={'Owner'} data={loadedNft.owner} />
+          <Section title={'Title'} data={decryptedNft.name} />
+          <Section title={'Description'} data={decryptedNft.description} />
+          <Section title={'Owner'} data={decryptedNft.owner} />
         </div>
         <div className="Button-Wrapper">
           {isViewingThumbnail ? (
@@ -72,7 +64,7 @@ export const Detail = () => {
               Return to Thumbnail
             </button>
           )}
-          {userAddress === loadedNft.owner && (
+          {userAddress === decryptedNft.owner && (
             <button className="Transfer-Button" onClick={handleOpen}>
               TRANSFER
             </button>
