477f83137a0ce67c240c0db44ae5ae57cd02f374
bibitto <bibitto.crypto@gmail.com>
2023-01-19 22:19:55 +0900

    update: add initVwbl

--- src/container/vwbl-container.js
+++ src/container/vwbl-container.js
@@ -1,10 +1,12 @@
 import { createContainer } from 'unstated-next';
 import { useState } from 'react';
 import Web3 from 'web3';
+import { ManageKeyType, UploadContentType, UploadMetadataType, VWBL } from 'vwbl-sdk';
 
 const useVWBL = () => {
   const [userAddress, setUserAddress] = useState('');
   const [web3, setWeb3] = useState();
+  const [vwbl, setVwbl] = useState();
 
   const connectWallet = async () => {
     try {
@@ -30,6 +32,9 @@ const useVWBL = () => {
           params: [{ chainId: web3.utils.toHex(properChainId) }],
         });
       }
+
+      // initVwblを実行してvwblインスタンスを作成する
+      initVwbl(web3);
     } catch (error) {
       if (error.code === 4001) {
         alert('Please connect Your Wallet.');
@@ -46,10 +51,25 @@ const useVWBL = () => {
   };
 
   // Lesson-3
-  const initVwbl = () => {};
+  const initVwbl = (web3) => {
+    // vwblインスタンスの作成
+    const vwblInstance = new VWBL({
+      web3,
+      contractAddress: process.env.REACT_APP_NFT_CONTRACT_ADDRESS,
+      vwblNetworkUrl: process.env.REACT_APP_VWBL_NETWORK_URL,
+      manageKeyType: ManageKeyType.VWBL_NETWORK_SERVER,
+      uploadContentType: UploadContentType.IPFS,
+      uploadMetadataType: UploadMetadataType.IPFS,
+      ipfsNftStorageKey: process.env.REACT_APP_NFT_STORAGE_KEY,
+    });
+    // vwblインスタンスをstateを保存
+    setVwbl(vwblInstance);
+    console.log(vwblInstance);
+  };
 
   return {
     userAddress,
+    vwbl,
     connectWallet,
     disconnectWallet,
     initVwbl,
