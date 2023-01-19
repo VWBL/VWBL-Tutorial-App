4a2b3e19bd38ef8155627572660c607e986e89f2
bibitto <bibitto.crypto@gmail.com>
2023-01-19 20:29:53 +0900

    chore: remove comment

--- src/container/vwbl-container.js
+++ src/container/vwbl-container.js
@@ -6,38 +6,25 @@ const useVWBL = () => {
   const [userAddress, setUserAddress] = useState('');
   const [web3, setWeb3] = useState();
 
-  // Lesson-2
   const connectWallet = async () => {
     try {
-      // windowオブジェクトからethereumオブジェクトを抽出
       const { ethereum } = window;
-
-      // ethereumオブジェクトの有無を確認
       if (!ethereum) {
         throw new Error('Please install MetaMask!');
       } else {
         console.log('MetaMask is installed!', ethereum);
       }
 
-      // ウォレットに接続
       await ethereum.enable();
-
-      // web3インスタンスの生成
       const web3 = new Web3(ethereum);
-
-      // ユーザーアドレスを取得
       const accounts = await web3.eth.getAccounts();
       const currentAccount = accounts[0];
-
-      // 各変数のstateを保存
       setWeb3(web3);
       setUserAddress(currentAccount);
 
-      // ネットワークを確認
       const connectedChainId = await web3.eth.getChainId();
-      const properChainId = parseInt(process.env.REACT_APP_CHAIN_ID); // 今回の場合、Mumbaiの80001
+      const properChainId = parseInt(process.env.REACT_APP_CHAIN_ID);
       if (connectedChainId !== properChainId) {
-        // ネットワークがMumbaiでない場合はネットワークを変更
         await ethereum.request({
           method: 'wallet_switchEthereumChain',
           params: [{ chainId: web3.utils.toHex(properChainId) }],
@@ -45,21 +32,15 @@ const useVWBL = () => {
       }
     } catch (error) {
       if (error.code === 4001) {
-        // ユーザーが接続を拒否したとき
         alert('Please connect Your Wallet.');
       } else {
-        // ethreumオブジェクトが確認できなかった時など
         alert(error.message);
       }
-
-      // エラー内容を開発者コンソールに表示
       console.error(error);
     }
   };
 
-  // Lesson-2
   const disconnectWallet = () => {
-    // 各変数のstateをリセット
     setUserAddress('');
     setWeb3(undefined);
   };
