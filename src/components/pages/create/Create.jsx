37122c1b8a1a98a57fe9518e884c2976d1390f80
bibitto <bibitto.crypto@gmail.com>
2023-01-20 13:08:52 +0900

    feat: update mintNft func

--- src/components/pages/create/Create.jsx
+++ src/components/pages/create/Create.jsx
@@ -5,6 +5,7 @@ import { useForm } from 'react-hook-form';
 import { ErrorMessage } from '@hookform/error-message';
 import './Create.css';
 import { useDisclosure } from '../../../hooks';
+import { VwblContainer } from '../../../container';
 
 export const Create = () => {
   const [file, setFile] = useState();
@@ -14,22 +15,52 @@ export const Create = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [isChecked, setIsChecked] = useState(false);
   const { isOpen, handleOpen } = useDisclosure();
+  const { web3, vwbl, connectWallet } = VwblContainer.useContainer();
 
   // Lesson-4
-  const mintNft = (data) => {
+  const mintNft = async (data) => {
     if (Object.keys(errors).length !== 0) {
       // エラーを確認
       console.log('errors', errors);
       return;
     }
+
+    // Loading開始
     setIsLoading(true);
-    console.log('submitted data', data);
-    console.log('mint start...');
-    setTimeout(() => {
-      console.log('mint success!');
+
+    // web3またはvwblインスタンスがundefinedの場合
+    if (!web3 || !vwbl) {
+      alert('Now your wallet is connected. Please try again.');
       setIsLoading(false);
+      await connectWallet();
+    }
+
+    // 各入力データを抽出
+    const { asset, thumbnail, title, description } = data;
+
+    try {
+      // VWBLネットワークに対する署名を確認
+      if (!vwbl.signature) {
+        await vwbl.sign();
+        return;
+      }
+
+      // VWBL NFTを発行
+      await vwbl.managedCreateTokenForIPFS(title, description, asset[0], thumbnail[0], 0);
+
+      // Loading終了
+      setIsLoading(false);
+
+      // Completeモーダルを表示
       handleOpen();
-    }, 7000);
+    } catch (error) {
+      // エラー内容を表示
+      console.log(error);
+      alert(error.message);
+
+      // Loading終了
+      setIsLoading(false);
+    }
   };
 
   const {
