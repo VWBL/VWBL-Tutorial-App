8f5d2e37bce0eb2e7b7111cdcaa13dd9b1ee106d
bibitto <bibitto.crypto@gmail.com>
2023-01-20 16:16:08 +0900

    chore: remove comments

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -3,7 +3,6 @@ import { VwblContainer } from '../../../container';
 import { TbWalletOff } from 'react-icons/tb';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import { ItemList } from '../../common';
-import { testNfts } from '../../../utils';
 import clsx from 'clsx';
 import './Home.css';
 import 'react-tabs/style/react-tabs.css';
@@ -14,57 +13,33 @@ export const Home = () => {
   const [ownedNfts, setOwnedNfts] = useState();
   const { userAddress, vwbl, web3, connectWallet, disconnectWallet } = VwblContainer.useContainer();
 
-  // Lesson-5
   const fetchNfts = async () => {
-    // userAddressまたはweb3またはvwblインスタンスがundefinedの場合は関数を終了
     if (!userAddress || !web3 || !vwbl) {
       console.log('Now your wallet is not connected. Please connect your wallet.');
       return;
     }
 
     try {
-      // ミントしたNFTのtokenIdの配列を取得
       const mintedTokenIds = await vwbl.getTokenByMinter(userAddress);
-
-      // mintedTokenIds内に値が存在する場合
       if (mintedTokenIds.length) {
-        // 取得したメタデータを保存する配列を定義
         const mintedNfts = [];
-
-        // mintedTokenIdsをもとにループ処理
         for (const tokenId of mintedTokenIds) {
-          // tokenIdからNFTメタデータを取得
           const metadata = await vwbl.getMetadata(tokenId);
-
-          // metadataが存在する場合はmintedNftsに追加
           if (metadata) mintedNfts.push(metadata);
         }
-
-        // 最新のNFTが一覧の上に来るように順番を入れ替えて保存
         setMintedNfts(mintedNfts.reverse());
+      }
 
-        // 所有しているNFTのtokenIdの配列を取得
-        const ownedTokenIds = await vwbl.getOwnTokenIds();
-
-        // ownedTokenIds内に値が存在する場合
-        if (ownedTokenIds.length) {
-          // 取得したメタデータを保存する配列を定義
-          const owendNfts = [];
-
-          // ownedTokenIdsをもとにループ処理
-          for (const tokenId of ownedTokenIds) {
-            // tokenIdからNFTメタデータを取得
-            const metadata = await vwbl.getMetadata(tokenId);
-
-            // metadataが存在する場合はowendNftsに追加
-            if (metadata) owendNfts.push(metadata);
-          }
-
-          setOwnedNfts(owendNfts.reverse());
+      const ownedTokenIds = await vwbl.getOwnTokenIds();
+      if (ownedTokenIds.length) {
+        const owendNfts = [];
+        for (const tokenId of ownedTokenIds) {
+          const metadata = await vwbl.getMetadata(tokenId);
+          if (metadata) owendNfts.push(metadata);
         }
+        setOwnedNfts(owendNfts.reverse());
       }
     } catch (error) {
-      // エラー内容を表示
       console.log(error);
     }
   };
