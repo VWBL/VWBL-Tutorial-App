6c223a8a9de8ff5a06ebf197bbb3cdd58cee8686
bibitto <bibitto.crypto@gmail.com>
2023-01-23 16:53:40 +0900

    feat: update transferNft func

--- src/hooks/transfer-nft.js
+++ src/hooks/transfer-nft.js
@@ -1,21 +1,51 @@
 import { useState } from 'react';
+import { VwblContainer } from '../container';
+import { useParams, useNavigate } from 'react-router-dom';
 
 export const useTransferNft = () => {
   const [isComplete, setIsComplete] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const { vwbl } = VwblContainer.useContainer();
+  const tokenId = Number(useParams().id);
+  const navigate = useNavigate();
 
   // Lesson-7
-  const transferNft = (data) => {
-    console.log('submitted data', data);
+  const transferNft = async (data) => {
+    // vwblが存在しない場合
+    if (!vwbl) {
+      console.log('Now your wallet is not connected. Please connect your wallet.');
+      return;
+    }
+
+    // フォームから受け取ったデータを抽出
+    const { address } = data;
+
+    //　Loadingモーダルを表示する
     setIsLoading(true);
-    setTimeout(() => {
-      setIsLoading(false);
+
+    try {
+      // VWBL NFTを送信
+      await vwbl.safeTransfer(address, tokenId);
+
+      //　Loadingモーダルを閉じる
+      setIsLoading(true);
+
+      // 送信完了モーダルを表示する
       setIsComplete(true);
-    }, 4000);
+    } catch (error) {
+      // エラー内容の表示
+      console.log(error);
+
+      //　Loadingモーダルを閉じる
+      setIsLoading(true);
+    }
   };
 
   const handleComplete = () => {
     setIsComplete((prev) => !prev);
+
+    // ホーム画面へ遷移させる
+    navigate('/');
   };
 
   return {
