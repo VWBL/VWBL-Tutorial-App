02ae2c3db25bc10ccf23ff2bda632d9802d7ebb8
hyde2000 <hyde2000s@gmail.com>
2023-01-25 19:06:36 +0900

    fix lint

--- src/components/pages/detail/Detail.jsx
+++ src/components/pages/detail/Detail.jsx
@@ -1,17 +1,18 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { VwblContainer } from "../../../container";
-import { useDisclosure } from "../../../hooks";
-import { decryptedImageData } from "../../../utils";
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { VwblContainer } from '../../../container';
+import { useDisclosure } from '../../../hooks';
+import { decryptedImageData } from '../../../utils';
 import {
   BackButton,
   CustomLoading,
   FileViewer,
   Section,
   TransferModal,
-} from "../../common";
-import { testNfts } from "../../../utils";
-import "./Detail.css";
+} from '../../common';
+import { testNfts } from '../../../utils';
+import './Detail.css';
 
 export const Detail = () => {
   const [decryptedNft, setDecryptedNft] = useState();
@@ -40,53 +41,53 @@ export const Detail = () => {
 
   if (!decryptedNft) {
     return (
-      <div style={{ height: "calc(100vh - 160px)" }}>
+      <div style={{ height: 'calc(100vh - 160px)' }}>
         <CustomLoading />
       </div>
     );
   }
 
   return (
-    <div style={{ height: "calc(100vh - 160px)", display: "flex" }}>
+    <div style={{ height: 'calc(100vh - 160px)', display: 'flex' }}>
       <TransferModal open={isOpen} onClose={handleOpen} nft={decryptedNft} />
       {isViewingThumbnail ? (
         <FileViewer
           url={decryptedNft.image}
-          alt="NFT"
-          height={"100%"}
-          width={"100%"}
+          alt='NFT'
+          height={'100%'}
+          width={'100%'}
         />
       ) : (
         <FileViewer
           url={decryptedNft.decrypted_image}
-          alt="NFT"
-          height={"100%"}
-          width={"100%"}
+          alt='NFT'
+          height={'100%'}
+          width={'100%'}
         />
       )}
-      <div className="Detail-Container">
-        <div className="Data-Wrapper">
-          <Section title={"Title"} data={decryptedNft.name} />
-          <Section title={"Description"} data={decryptedNft.description} />
-          <Section title={"Owner"} data={decryptedNft.owner} />
+      <div className='Detail-Container'>
+        <div className='Data-Wrapper'>
+          <Section title={'Title'} data={decryptedNft.name} />
+          <Section title={'Description'} data={decryptedNft.description} />
+          <Section title={'Owner'} data={decryptedNft.owner} />
         </div>
-        <div className="Button-Wrapper">
+        <div className='Button-Wrapper'>
           {isViewingThumbnail ? (
-            <button className="View-Button" onClick={handleViewData}>
+            <button className='View-Button' onClick={handleViewData}>
               View Data
             </button>
           ) : (
-            <button className="View-Button" onClick={handleViewData}>
+            <button className='View-Button' onClick={handleViewData}>
               Return to Thumbnail
             </button>
           )}
           {userAddress === decryptedNft.owner && (
-            <button className="Transfer-Button" onClick={handleOpen}>
+            <button className='Transfer-Button' onClick={handleOpen}>
               TRANSFER
             </button>
           )}
         </div>
-        <BackButton to={"/"} />
+        <BackButton to={'/'} />
       </div>
     </div>
   );
