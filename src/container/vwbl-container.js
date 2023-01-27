6a6fff5237d6be31734ac9812f19beae960efa87
bibitto <bibitto.crypto@gmail.com>
2023-01-27 12:54:11 +0900

    fix: change metamask-connect logic

--- src/container/vwbl-container.js
+++ src/container/vwbl-container.js
@@ -20,7 +20,7 @@ const useVWBL = () => {
       }
 
       // ウォレットに接続
-      await ethereum.enable();
+      await ethereum.send('eth_requestAccounts');
 
       // web3インスタンスの生成
       const web3 = new Web3(ethereum);
