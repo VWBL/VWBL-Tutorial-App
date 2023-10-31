4bb5170663472ad91ae55f9ca267f6898f8ae1ab
seki <schokolade0317@gmail.com>
2023-10-31 22:14:06 +0900

    ビルド通った

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,19 @@
-module.exports = function override(config) {
+const webpack = require('webpack');
+
+module.exports = function override(config, env) {
   config.resolve.fallback = {
     fs: false,
     os: false,
     https: false,
     http: false,
     crypto: false,
+    path: false,
+    child_process: false,
+    stream: false
   };
+  config.plugins.push(new webpack.DefinePlugin({
+    ...env.stringified,
+    'process.env.FLUENTFFMPEG_COV': false
+  }));
   return config;
 };
